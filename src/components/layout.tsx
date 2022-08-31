2b7d7959af836e0e915bae15b4ff742dae36dda1
emilyraddatz <raddatz.em@gmail.com>
2022-08-31 13:56:47 -0400

    nav bar added

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import { Outlet, useNavigate } from 'react-router-dom';
 import { useAuthenticator, Button} from '@aws-amplify/ui-react';
+import "./navbar.css"
  
 //https://ui.docs.amplify.aws/react/guides/auth-protected
 export function Layout() {
@@ -17,18 +18,21 @@ export function Layout() {
  }
  return (
    <>
-     <nav>
-       <Button onClick={() => navigate('/')}>Home</Button>
-       <Button onClick={() => navigate('/timeline')}>
-         First Protected Route
+     <nav className="NavbarItems">
+     <h1 className="navbar-logo">Disney Social</h1>
+     <img className='icon' src=""></img>
+       <Button className='nav-links' onClick={() => navigate('/')}>Profile</Button>
+       
+       <Button className='nav-links'onClick={() => navigate('/timeline')}>
+         Timeline
        </Button>
-       <Button onClick={() => navigate('/timeline')}>
+       <Button className='nav-links' onClick={() => navigate('/timeline')}>
          Second Protected Route
        </Button>
        {route !== 'authenticated' ? (
-         <Button onClick={() => navigate('/login')}>Login</Button>
+         <Button className='nav-links' onClick={() => navigate('/login')}>Login</Button>
        ) : (
-         <Button onClick={() => logOut()}>Logout</Button>
+         <Button className='nav-links' onClick={() => logOut()}>Logout</Button>
        )}
      </nav>
      <Outlet />
