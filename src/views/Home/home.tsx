bc3f221828fbf2eba718e8fc361a6b15b2035bc8
emilyraddatz <raddatz.em@gmail.com>
2022-09-02 09:03:02 -0400

    removed again

--- src/views/Home/home.tsx
+++ src/views/Home/home.tsx
@@ -7,18 +7,11 @@ import axios from "axios"
 
 const Home = () => {
  const [userName, setUserName] = useState("")
-//  const [weather, setWeather] = useState([])
+
  Auth.currentUserInfo().then(obj => setUserName(obj.username))
 
 
-//  useEffect(() => {
-//     const getWeather = async () => {
-//         const response = await axios.get('http://api.openweathermap.org/geo/1.0/zip?zip=32836,usa&appid=7eb1fbc6f3960c4f27269ae75663ee0f')
-//         setWeather(response.data.weather)
-//     }
-//     getWeather()
-//     console.log(getWeather)
-// }, [])
+
 
 
 
@@ -27,7 +20,7 @@ const Home = () => {
     <div className="card__content">
         {userName}
        
-        {/* {weather} */}
+
     </div>
   
    </div>
